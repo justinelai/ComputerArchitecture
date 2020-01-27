afd9f0a40f60c179ac95e141371f7839b851f8a8
justinelai <justine.lai@gmail.com>
2020-01-27 18:14:14 -0500

    working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,28 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.reg = [0] * 8
+        self.ram = [0] * 256 #256 bytes of memory
 
-
-        #8 general-purpose 8-bit numeric registers R0-R7.
-        #R5 is reserved as the interrupt mask (IM)
-        #R6 is reserved as the interrupt status (IS)
-        #R7 is reserved as the stack pointer (SP)
-        #Only hold values between 0-255. After performing math on registers in the emulator, bitwise-AND the result with 0xFF (255) to keep the register values in that range.
-        
-        self.reg = [0b0] * 8
-        self.ram = [0b0] * 2**8 #256 bytes of memory
         self.pc = 0 # Program Counter, address of the currently executing instruction
-        self.mar = None # Memory Address Register (MAR) contains the address that is being read or written to.
-        self.mdr = None # Memory Data Register (MDR) contains the data that was read or the data to write. 
-        # You don't need to add the MAR or MDR to your CPU class, but they would make handy parameter names for ram_read() and ram_write(), if you wanted.
-        self.ir = None # Instruction register contains a copy of the currently executing instruction
-
-        self.opcodes = {
-            0b10000010: "LDI",
-            0b01000111: "PRN",
-            0b00000001: "HLT"
-        }
-
 
     def load(self):
         """Load a program into memory."""
@@ -51,13 +33,6 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
-    def ram_read(self, address):
-        # should accept the address to read and return the value stored there.
-        return self.ram[address]
-       
-    def ram_write(self, value, address):
-        # should accept a value to write, and the address to write it to.
-        return self.ram[address] = value
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -88,19 +63,37 @@ class CPU:
 
         print()
 
+    def ram_read(self, addr):
+        # should accept the address to read and return the value stored there.
+        return self.ram[addr]
+       
+    def ram_write(self, addr, value):
+        # should accept a value to write, and the address to write it to.
+        self.ram[addr] = value
+
     def run(self):
         """Run the CPU."""
-        """
-        It needs to read the memory address that's stored in register PC, and store that result in IR, the Instruction Register. This can just be a local variable in run(). 
-        """
-        running = True
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
         print("Running CPU...")
-        while True:
-            self.ir = self.ram_read(self.pc)
-            opcode = self.opcodes[self.ir]
-            if opcode has two args
-                operand_a = self. [self.pc + 1]
-                operand_b = 
-            elif opcode has one arg
-                operand_a = 
+        running = True
+        while running:
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == HLT:
+                sys.exit(1) # Error exit status
+            elif ir == LDI:
+                # LDI register immediate - Set the value of a register to an integer.
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == PRN:
+                # PRN register pseudo-instruction - Print numeric value stored in the given register.
+                # Print to the console the decimal integer value that is stored in the given register.
+                print(int(self.reg[operand_a]))
+                self.pc += 2
             else:
+                self.pc += 1
